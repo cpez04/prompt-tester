45e6d5dfa6307cc332d6b023a69c7ac73a51b089
CHRISTOPHER PEREZ <CHRISTOPHERPEREZ@COLLEGE.HARVARD.EDU>
2025-04-19 18:21:16 -0400

    Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Layout>{children}</Layout>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
