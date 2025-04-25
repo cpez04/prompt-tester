7da4614556e51c0c759d35b185e4b499654ce60c
CHRISTOPHER PEREZ <CHRISTOPHERPEREZ@COLLEGE.HARVARD.EDU>
2025-04-25 11:10:21 -0400

    Feedback - if logged in

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -57,15 +57,17 @@ export default function ProfileIcon({ user, loading }: ProfileIconProps) {
 
   return (
     <div className="flex items-center gap-4 relative" ref={dropdownRef}>
-      {/* Feedback Link to the left */}
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLScAsZIcJ5WVQBnqxBLA_VFWlRoj8SpgHYC0fPaGNNWMq4FDkA/viewform?usp=dialog"
-        target="_blank"
+      {/* Feedback Link to the left, if user is logged in */}
+      {user && (
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScAsZIcJ5WVQBnqxBLA_VFWlRoj8SpgHYC0fPaGNNWMq4FDkA/viewform?usp=dialog"
+          target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-base-content/70 hover:text-base-content transition-colors"
-      >
-        Got Feedback?
-      </a>
+        >
+          Got Feedback?
+        </a>
+      )}
 
       {/* Profile Icon */}
       {loading ? (
